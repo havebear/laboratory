6d31b311bf4725b25f843012e4411ad0a276b290
havebear <616749285@qq.com>
2021-12-17 14:08:10 +0800

    chore: 完善组合算法注释

--- 算法/组合/index.js
+++ 算法/组合/index.js
@@ -2,7 +2,7 @@
  * @Author: BGG
  * @Date: 2021-12-17 13:04:06
  * @LastEditors: BGG
- * @LastEditTime: 2021-12-17 14:03:48
+ * @LastEditTime: 2021-12-17 14:07:34
  * @Description:  组合算法，n个元素取m个元素的所有组合情况(非排列)
  */
 
@@ -47,7 +47,7 @@ const combo = (arr, num) => {
     if (num === _result.length) {
       result.push(_result)
     } else {
-      // 循环次数，等于传入数组长度 + 子集长度 + 当前子集长度
+      // 循环次数，等于传入数组长度 - 子集长度 + 当前子集长度
       const len = _arr.length - num + _result.length
       // 
       for (let i = 0; i <= len; i++) {
